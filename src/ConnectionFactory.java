edc82b3f3b9c9fa77c767e5cb8c50e34cdae5416
Rafael <rafaborgesdasilva75@gmail.com>
2026-05-01 00:51:55 -0300

    first commit

---
+++ src/ConnectionFactory.java
@@ -0,0 +1,27 @@
+import java.net.ConnectException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+public class ConnectionFactory {
+    public static Connection getConnection() throws Exception{
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String login = "root";
+            String senha = "";
+            String url = "jdbc:mysql://localhost:3306/";
+            return DriverManager.getConnection(url, login, senha);
+        } catch (Exception e){
+            throw new Exception(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        try{
+            Connection connect = ConnectionFactory.getConnection();
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
