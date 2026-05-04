60c9de0794355cfa88824ef620c80b0fced63775
Rafael Borges <rafaborgesdasilva75@gmail.com>
2026-05-04 20:37:47 -0300

    refactor: alterado DAO inicializando a conexao com o banco MySQL

--- src/dao/NotaDAO.java
+++ src/dao/NotaDAO.java
@@ -1,5 +1,27 @@
 package dao;
 
+import models.Aluno;
+import main.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class NotaDAO {
+    private Aluno aluno;
+    private Connection connection;
+    private PreparedStatement preparedStatement; // deixa rodar querys
+    private ResultSet resultSet; // seria a tabela ou o retorno da query
+
+    public NotaDAO() throws Exception{
+        try{
+            this.connection = ConnectionFactory.getConnection();
+        } catch (Exception e){
+            throw new Exception(">> ERRO ->" + e.getMessage());
+        }
+    }
+
     
 }
