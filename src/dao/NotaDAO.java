4abedbbd8e8feb58790c6e65f2b3f99b62d90e75
Rafael <rafaborgesdasilva75@gmail.com>
2026-05-07 23:47:11 -0300

    refactor: alterado query insert para igualar a tabela do banco de dados

--- src/dao/NotaDAO.java
+++ src/dao/NotaDAO.java
@@ -24,15 +24,17 @@ public class NotaDAO {
 
     public void salvar() throws Exception{
         try{
-            String sql = "INSERT INTO nota (disciplina, semestre, nota, faltas)" + 
-                        "values (?, ?, ?, ?);";
+            String sql = "INSERT INTO nota (aluno_id, disciplina, semestre, nota, faltas)" + 
+                        "values (?, ?, ?, ?, ?);";
 
             preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, nota.getDisciplina());
-            preparedStatement.setString(2, nota.getSemestre());
-            preparedStatement.setDouble(3, nota.getNota());
-            preparedStatement.setInt(4, nota.getFalta());
-            preparedStatement.setInt(5, nota.getAlunoId());
+            
+            preparedStatement.setInt(1, nota.getAlunoId());
+            preparedStatement.setString(2, nota.getDisciplina());
+            preparedStatement.setString(3, nota.getSemestre());
+            preparedStatement.setDouble(4, nota.getNota());
+            preparedStatement.setInt(5, nota.getFalta());
+            
             preparedStatement.executeUpdate();
         } catch(Exception e){
             throw new Exception(">> ERRO AO SALVAR ->" + e.getMessage());
@@ -47,11 +49,12 @@ public class NotaDAO {
             preparedStatement = connection.prepareStatement(sql);
             resultSet = preparedStatement.executeQuery();
             while(resultSet.next()){
+                int alunoId = resultSet.getInt("aluno_id");
                 String disciplina = resultSet.getString("disciplina");
                 String semestre = resultSet.getString("semestre");
                 double notaAluno = resultSet.getDouble("nota");
                 int falta = resultSet.getInt("faltas");
-                int alunoId = resultSet.getInt("aluno_id");
+                
 
                 lista.add(new Nota(notaAluno, disciplina, falta, semestre, alunoId));
             }
